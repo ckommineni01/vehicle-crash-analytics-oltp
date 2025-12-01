497c614c27a5ea8a40ddf9b8045afacb6e527377
Chaitanya Krishna Kommineni <komminenichaitanyakrishna2002@gmail.com>
2025-12-01 12:06:47 -0500

    Update indexes_phase2.sql

--- sql/indexes_phase2.sql
+++ sql/indexes_phase2.sql
@@ -1,1 +1,9 @@
+CREATE INDEX idx_collision_factors_factor
+    ON collision_factors (factor_id);
+
+CREATE INDEX idx_collision_factors_collision
+    ON collision_factors (collision_id);
+
+CREATE INDEX idx_collisions_date_borough
+    ON collisions (crash_date, borough_id);
 
